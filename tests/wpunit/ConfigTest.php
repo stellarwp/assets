f098869c0e0deb127e5ec2a260222c756f03e575
Matthew Batchelder <borkweb@gmail.com>
2023-08-23 11:31:56 -0400

    Add more tests for config

--- tests/wpunit/ConfigTest.php
+++ tests/wpunit/ConfigTest.php
@@ -22,4 +22,36 @@ class ConfigTest extends AssetTestCase {
 
 		$this->assertEquals( 'bork', Config::get_hook_prefix() );
 	}
+
+	/**
+	 * @test
+	 */
+	public function should_set_path() {
+		Config::set_path( dirname( dirname( __DIR__ ) ) );
+
+		$this->assertEquals( dirname( dirname( __DIR__ ) ), Config::get_path() );
+	}
+
+	/**
+	 * @test
+	 */
+	public function should_set_version() {
+		Config::set_version( '1.1.0' );
+
+		$this->assertEquals( '1.1.0', Config::get_version() );
+	}
+
+	/**
+	 * @test
+	 */
+	public function should_reset() {
+		Config::set_hook_prefix( 'bork' );
+		Config::set_version( '1.1.0' );
+		Config::set_path( dirname( dirname( __DIR__ ) ) );
+		Config::reset();
+
+		$this->assertEquals( null, Config::get_hook_prefix() );
+		$this->assertEquals( null, Config::get_path() );
+		$this->assertEquals( null, Config::get_version() );
+	}
 }
