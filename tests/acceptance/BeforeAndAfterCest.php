290f4cd615234a2ec437c9a9bae902ffa88fff90
Matthew Batchelder <borkweb@gmail.com>
2023-08-24 12:13:31 -0400

    Add before and after tests, more js tests

---
+++ tests/acceptance/BeforeAndAfterCest.php
@@ -0,0 +1,52 @@
+<?php
+namespace StellarWP\Assets\Tests;
+
+use AcceptanceTester;
+
+class BeforeAndAfterCest {
+	public function it_should_call_callback_after_enqueue( AcceptanceTester $I ) {
+		$code = file_get_contents( codecept_data_dir( 'enqueue-template.php' ) );
+		$code .= <<<PHP
+		add_action( 'wp_enqueue_scripts', function() {
+			Asset::register( 'fake-js', 'fake.js' )
+				->set_action( 'wp_enqueue_scripts' )
+				->call_after_enqueue( function() {
+					\$_SERVER['fake-js-after-enqueue'] = true;
+				} )
+				->enqueue();
+		}, 100 );
+
+		add_action( 'wp_footer', function() {
+			if ( ! empty( \$_SERVER['fake-js-after-enqueue'] ) ) {
+				echo '<strong data-fired="yes">FIRED</strong>';
+			}
+		} );
+		PHP;
+
+		$I->haveMuPlugin( 'enqueue.php', $code );
+
+
+		$I->amOnPage( '/' );
+		$I->seeElement( 'strong', [ 'data-fired' => 'yes' ] );
+	}
+
+	public function it_should_print_before_and_after( AcceptanceTester $I ) {
+		$code = file_get_contents( codecept_data_dir( 'enqueue-template.php' ) );
+		$code .= <<<PHP
+		add_action( 'wp_enqueue_scripts', function() {
+			Asset::register( 'fake-js', 'fake.js' )
+				->set_action( 'wp_enqueue_scripts' )
+				->print_before( '<strong data-before="yes"></strong>' )
+				->print_after( '<strong data-after="yes"></strong>' )
+				->enqueue();
+		}, 100 );
+		PHP;
+
+		$I->haveMuPlugin( 'enqueue.php', $code );
+
+
+		$I->amOnPage( '/' );
+		$I->seeElement( 'strong', [ 'data-before' => 'yes' ] );
+		$I->seeElement( 'strong', [ 'data-after' => 'yes' ] );
+	}
+}
--- tests/acceptance/EnqueueJSCest.php
+++ tests/acceptance/EnqueueJSCest.php
@@ -2,6 +2,7 @@
 namespace StellarWP\Assets\Tests;
 
 use AcceptanceTester;
+use PHPUnit\Framework\Assert;
 
 class EnqueueJSCest {
 	public function it_should_register_and_enqueue_js( AcceptanceTester $I ) {
@@ -19,6 +20,24 @@ class EnqueueJSCest {
 
 		$I->amOnPage( '/' );
 		$I->seeElement( 'script', [ 'src' => 'http://wordpress.test/wp-content/plugins/assets/tests/_data/js/fake.js?ver=1.0.0' ] );
+	}
+
+	public function it_should_not_enqueue_if_dependencies_missing( AcceptanceTester $I ) {
+		$code = file_get_contents( codecept_data_dir( 'enqueue-template.php' ) );
+		$code .= <<<PHP
+		add_action( 'wp_enqueue_scripts', function() {
+			Asset::register( 'fake-js', 'fake.js' )
+				->set_dependencies( [ 'something' ] )
+				->set_action( 'wp_enqueue_scripts' )
+				->enqueue();
+		}, 100 );
+		PHP;
+
+		$I->haveMuPlugin( 'enqueue.php', $code );
+
+
+		$I->amOnPage( '/' );
+		$I->dontSeeElement( 'script', [ 'src' => 'http://wordpress.test/wp-content/plugins/assets/tests/_data/js/fake.js?ver=1.0.0' ] );
 	}
 
 	public function it_should_enqueue_with_custom_version( AcceptanceTester $I ) {
@@ -71,5 +90,59 @@ class EnqueueJSCest {
 
 		$I->amOnPage( '/' );
 		$I->dontSeeElement( 'script', [ 'src' => 'http://wordpress.test/wp-content/plugins/assets/tests/_data/js/fake.js?ver=1.0.0' ] );
+	}
+
+	public function it_should_be_deferred( AcceptanceTester $I ) {
+		$code = file_get_contents( codecept_data_dir( 'enqueue-template.php' ) );
+		$code .= <<<PHP
+		add_action( 'wp_enqueue_scripts', function() {
+			Asset::register( 'fake-js', 'fake.js' )
+				->set_as_deferred( true )
+				->set_action( 'wp_enqueue_scripts' )
+				->enqueue();
+		}, 100 );
+		PHP;
+
+		$I->haveMuPlugin( 'enqueue.php', $code );
+
+
+		$I->amOnPage( '/' );
+		$I->seeElement( '#fake-js-js[defer]' );
+	}
+
+	public function it_should_be_async( AcceptanceTester $I ) {
+		$code = file_get_contents( codecept_data_dir( 'enqueue-template.php' ) );
+		$code .= <<<PHP
+		add_action( 'wp_enqueue_scripts', function() {
+			Asset::register( 'fake-js', 'fake.js' )
+				->set_as_async( true )
+				->set_action( 'wp_enqueue_scripts' )
+				->enqueue();
+		}, 100 );
+		PHP;
+
+		$I->haveMuPlugin( 'enqueue.php', $code );
+
+
+		$I->amOnPage( '/' );
+		$I->seeElement( '#fake-js-js[async]' );
+	}
+
+	public function it_should_be_a_module( AcceptanceTester $I ) {
+		$code = file_get_contents( codecept_data_dir( 'enqueue-template.php' ) );
+		$code .= <<<PHP
+		add_action( 'wp_enqueue_scripts', function() {
+			Asset::register( 'fake-js', 'fake.js' )
+				->set_as_module( true )
+				->set_action( 'wp_enqueue_scripts' )
+				->enqueue();
+		}, 100 );
+		PHP;
+
+		$I->haveMuPlugin( 'enqueue.php', $code );
+
+
+		$I->amOnPage( '/' );
+		$I->seeElement( '#fake-js-js[type=module]' );
 	}
 }
