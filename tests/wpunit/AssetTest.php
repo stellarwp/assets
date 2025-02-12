d8f5b06239b34beea06406800f998669d903c6e7
Jeremy Pry <jeremy.pry@gmail.com>
2025-02-12 10:11:45 -0600

    Add unit test for the filter

--- tests/wpunit/AssetTest.php
+++ tests/wpunit/AssetTest.php
@@ -29,4 +29,31 @@ class AssetTest extends AssetTestCase {
 
 		$this->assertEquals( WP_PLUGIN_DIR . '/assets/tests/_data/', $asset->get_root_path() );
 	}
+
+	public function test_can_filter_asset_version(): void {
+		$prefix = 'bork';
+
+		Config::reset();
+		Config::set_hook_prefix( $prefix );
+		Config::set_version( '1.1.0' );
+		Config::set_path( constant( 'WP_PLUGIN_DIR' ) . '/assets' );
+
+		$asset = new Asset( 'test-script', 'fake.js', '1.0.0', codecept_data_dir() );
+
+		// The asset version should be the same as the one passed to the constructor.
+		$this->assertEquals( '1.0.0', $asset->get_version() );
+
+		// Filter the asset version.
+		$filter = function () {
+			return '2.0.0';
+		};
+
+		add_filter( "stellarwp/assets/{$prefix}/version", $filter );
+
+		// After filtering, the asset version should be updated.
+		$this->assertEquals( '2.0.0', $asset->get_version() );
+
+		// Cleanup the filter.
+		remove_filter( "stellarwp/assets/{$prefix}/version", $filter );
+	}
 }
